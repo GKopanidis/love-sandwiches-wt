504d404c157565d9cb0f8b163a8908802af8393c
Georgios Kopanidis <kopanidis@me.com>
2023-11-28 11:50:28 +0000

    Fix typos in docstring

--- run.py
+++ run.py
@@ -14,13 +14,13 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     """
     print("Please enter sales data from the last market.")
-    print("Data should be six numbers, seperated by commas.")
+    print("Data should be six numbers, separated by commas.")
     print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your Data here: ")
+    data_str = input("Enter your data here: ")
 
     sales_data = data_str.split(",")
     validate_data(sales_data)
@@ -39,6 +39,6 @@ def validate_data(values):
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"invalid data: {e}, please try again.\n")
+        print(f"Invalid data: {e}, please try again.\n")
 
 get_sales_data()
